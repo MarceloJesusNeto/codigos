dbea17eee797cf0463e0419dd70c7606887320d2
Marcelo de Jesus <marcelo.j.p.s.neto@gmail.com>
2023-08-02 17:33:23 -0300

    subindo arquivos da primeira aula de java

---
+++ Olámundo/src/principal/OlaMundo.java
@@ -0,0 +1,19 @@
+package principal;
+//classe pricipal
+public class OlaMundo {
+	//metodo pricipal, onde execulta o programa
+	public static void main(String[] args) {
+		
+		//imprime uma linha e ao final salta uma nova linha
+		//print = imprimir; in = new line;
+		System.out.println("hello world, olá mundo");
+		
+		//comentario de uma linha
+		/*
+		 * esse é 
+		 * um comentario
+		 * de multiplas linhas
+		 */
+	}
+
+}
---
+++ Variaveis/src/principal/Variaveis.java
@@ -0,0 +1,51 @@
+package principal;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		int numero;
+		numero = 10;
+
+		if (numero >= 10) {
+			System.out.println("aprovado");
+		} else {
+			System.out.println("reprovado");
+		}
+
+		System.out.println(numero);
+		double decimal = 10.50;
+		double testeNovoNumero = 10;
+		int testeNumero = (int) 50.25;
+
+		// IMPORTANTE
+		// aspas duplas para string
+		String nome = "Marcelo de Jesus";
+		// aspas simples para char
+		char sexo = 'M';
+		// booleanos = valores verdadeiros ou falssos
+		boolean verdadeiro = true;
+		boolean falso = false;
+
+		System.out.println(testeNovoNumero);
+		System.out.println(testeNumero);
+		System.out.println(sexo);
+		System.out.println(nome);
+		System.out.print(verdadeiro);
+		System.out.println(falso);
+		System.out.print("nome:");
+		System.out.println("Marcelo M");
+		
+		/*para oranizar o odigo usamos o
+		 * crtl + shift + F
+		 */
+
+		int anos = 17;
+		
+		System.out.println("*********************");
+		
+		System.out.print("Meu nome e:" + nome + "tenho" + " " + anos + " " + "anos");
+		
+		
+	}
+
+}
